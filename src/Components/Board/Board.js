9075499fa145fd8cc18d40d2f352780be08b1e25
time2fishman <adam_h_martinez@hotmail.com>
2024-01-12 16:52:25 -0700

    Split components into separte files. Style

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
-const Square = ({ value, onSquareClick }) => {
-  return <button className="square" onClick={onSquareClick}>{value}</button>
-}
+import Square from "../Square/Square";
 
 const Board = ({ xIsNext, squares, onPlay }) => {
   const handleClick = (i) => {
@@ -48,48 +44,6 @@ const Board = ({ xIsNext, squares, onPlay }) => {
   )
 }
 
-  const Game = () => {
-  const [history, setHistory] = useState([Array(9).fill(null)])
-  const [currentMove, setCurrentMove] = useState(0)
-  const xIsNext = currentMove % 2 === 0
-  const currentSquares = history[currentMove]
-
-  const handlePlay = (nextSquares) => {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
-    setHistory(nextHistory)
-    setCurrentMove(nextHistory.length - 1)
-  }
-
-  const jumpTo = (nextMove) => {
-    setCurrentMove(nextMove)
-  }
-
-  const moves = history.map((squares, move) => {
-    let description
-    if (move > 0) {
-      description = "Go to move #" + move
-    } else {
-      description = "Go to game start"
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    )
-  })
-
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
-      </div>
-    </div>
-  )
-}
-
 const calculateWinner = (squares) => {
   const lines = [
     [0, 1, 2],
@@ -111,4 +65,4 @@ const calculateWinner = (squares) => {
   return null
 }
 
-export default Game
+export default Board
---
+++ src/Components/Square/Square.js
@@ -0,0 +1,5 @@
+const Square = ({ value, onSquareClick }) => {
+  return <button className="square" onClick={onSquareClick}>{value}</button>
+}
+
+export default Square
